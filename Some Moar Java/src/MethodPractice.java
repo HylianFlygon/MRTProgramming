7615e1e519b2f70e5cbb1e05c5d44bd8ac58f10b
mhs <mhs@MHS-Science3-02>
2017-10-25 17:14:07 -0700

    fsds

---
+++ Some Moar Java/src/MethodPractice.java
@@ -0,0 +1,13 @@
+public class MethodPractice {
+	public static void main(String[] args){
+		
+	}
+	public double max(double first, double second){
+		if(first > second){
+			return first;
+		}else{
+			return second;
+		}
+	}
+	
+}
